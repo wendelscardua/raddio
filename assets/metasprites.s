71980fbd0c194e69c15b3db238e1b84bfaac63cc
Wendel Scardua <wendelscardua@gmail.com>
2020-06-17 12:32:33 -0300

    Better, colorful arrows

--- assets/metasprites.s
+++ assets/metasprites.s
@@ -8,26 +8,26 @@ metasprite_0_data:
 
 metasprite_1_data:
 
-	.byte   0,  0,$02,2
-	.byte   8,  0,$03,2
-	.byte   0,  8,$12,2
-	.byte   8,  8,$13,2
+	.byte   0,  0,$02,1
+	.byte   8,  0,$03,1
+	.byte   0,  8,$12,1
+	.byte   8,  8,$13,1
 	.byte 128
 
 metasprite_2_data:
 
-	.byte   0,  0,$04,2
-	.byte   8,  0,$05,2
-	.byte   0,  8,$14,2
-	.byte   8,  8,$15,2
+	.byte   0,  0,$04,3
+	.byte   8,  0,$05,3
+	.byte   0,  8,$14,3
+	.byte   8,  8,$15,3
 	.byte 128
 
 metasprite_3_data:
 
-	.byte   0,  0,$06,2
-	.byte   8,  0,$07,2
-	.byte   0,  8,$16,2
-	.byte   8,  8,$17,2
+	.byte   0,  0,$06,0
+	.byte   8,  0,$07,0
+	.byte   0,  8,$16,0
+	.byte   8,  8,$17,0
 	.byte 128
 
 metasprite_pointers:
