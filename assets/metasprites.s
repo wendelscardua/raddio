3d21b8d7523aec70936062377c59bccc4fcc5d83
Wendel Scardua <wendelscardua@gmail.com>
2020-06-23 00:30:33 -0300

    Grades metasprites

--- assets/metasprites.s
+++ assets/metasprites.s
@@ -30,9 +30,40 @@ metasprite_3_data:
 	.byte   8,  8,$17,0
 	.byte 128
 
+metasprite_4_data:
+
+	.byte - 2,  0,$21,2
+	.byte   6,  0,$22,2
+	.byte  14,  0,$23,2
+	.byte 128
+
+metasprite_5_data:
+
+	.byte   0,  0,$31,1
+	.byte   8,  0,$32,1
+	.byte  16,  0,$33,1
+	.byte 128
+
+metasprite_6_data:
+
+	.byte   1,  0,$41,0
+	.byte   9,  0,$42,0
+	.byte 128
+
+metasprite_7_data:
+
+	.byte   0,  0,$51,3
+	.byte   8,  0,$52,3
+	.byte 128
+
 metasprite_pointers:
 
 	.word metasprite_0_data
 	.word metasprite_1_data
 	.word metasprite_2_data
 	.word metasprite_3_data
+	.word metasprite_4_data
+	.word metasprite_5_data
+	.word metasprite_6_data
+	.word metasprite_7_data
+
